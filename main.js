efbb70a80fa76e26724c67c20a67dd7200b7c964
Tanjil Haque <tanjilsmacbook@Tanjils-MacBook-Air.local>
2024-12-15 00:33:34 +0600

    random color genertor using dom

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-const obj = { name: "John", age: 30 };
-for (let key in obj) {
-  console.log(key, obj[key]);
+const title = document.createElement("h1");
+title.innerText = "Random Color generator using DOM"
+document.body.appendChild(title);
+
+const colorChangeBtn = document.createElement("button");
+colorChangeBtn.innerText = "Change Color";
+document.body.appendChild(colorChangeBtn);
+
+function rng(){
+    let randomNum = Math.floor(Math.random() * 256);
+    return randomNum;
 }
+
+colorChangeBtn.addEventListener("click", function(){
+    document.body.style.backgroundColor = `rgba(${rng()}, ${rng()}, ${rng()})`;
+    console.log(`${rng()}, ${rng()}, ${rng()}`);
+})
